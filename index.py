94a57097105e141cb0cdc8ada9f000932b31c4e2
Michael Halpin <halpinm2010@gmail.com>
2022-04-20 19:31:34 +0100

    .

--- index.py
+++ index.py
@@ -57,9 +57,10 @@ else:
                     elif user.messages(all=False, limit=int(argv[2]), offset=0, filter="all")[0][x]['comment_type'] == 2:
                         print("replied to your comment in the studio,", end=" ")
                     else:
-                        print("ERROR: scl error, unhandled comment type.")
+                        print("ERROR: scl error, unhandled comment type. Please report this at https://scratch.mit.edu/users/applejuiceproduc")
                         quit()
-                    print(user.messages(all=False, limit=int(argv[2]), offset=0, filter="all")[0][x]['comment_fragment'], end="\n\n")
+                    print(user.messages(all=False, limit=int(argv[2]), offset=0, filter="all")[0][x]["comment_obj_title"], end=" ")
+                    print("\"" + user.messages(all=False, limit=int(argv[2]), offset=0, filter="all")[0][x]['comment_fragment'] + "\"", end="\n\n")
                 #print(user.messages(all=False, limit=int(argv[2]), offset=0, filter="all")[0][0], end=" ")
             else:
                 print(user.messages(all=False, limit=user.messages_count(), offset=0, filter="all"))
