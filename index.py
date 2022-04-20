075d5ad68a125768e7287f09ffa62b722a5f5a07
Michael Halpin <halpinm2010@gmail.com>
2022-04-20 15:29:16 +0100

    .

--- index.py
+++ index.py
@@ -1,24 +1,27 @@
 import scratchconnect
 from sys import argv
-if argv[1] == "sign-in":
-    if argv.__len__() == 4:
-        try:
-            user = scratchconnect.ScratchConnect(argv[2], argv[3])
-            userw = open("signed-in-user", 'w')
-            userw.write(argv[2] + " " + argv[3])
-            userw.close()
-        except:
-            print("ERROR: Incorrect password or username.")
+if argv.__len__() == 1:
+    pass
+else:
+    if argv[1] == "sign-in":
+        if argv.__len__() == 4:
+            try:
+                user = scratchconnect.ScratchConnect(argv[2], argv[3])
+                userw = open("signed-in-user", 'w')
+                userw.write(argv[2] + " " + argv[3])
+                userw.close()
+            except:
+                print("ERROR: Incorrect password or username.")
+                quit()
+        else:
+            print("ERROR: Incorrect usage, scl sign-in <username> <password>.")
             quit()
     else:
-        print("ERROR: Incorrect usage, scl sign-in <username> <password>.")
-        quit()
-else:
-    try:
-        userr = open("signed-in-user", 'r')
-        usert = userr.read().split()
-        userr.close()
-        user = scratchconnect.ScratchConnect(usert[0], usert[1])
-    except:
-        print("ERROR: You need to be signed in to use SCL.")
-        quit()
+        try:
+            userr = open("signed-in-user", 'r')
+            usert = userr.read().split()
+            userr.close()
+            user = scratchconnect.ScratchConnect(usert[0], usert[1])
+        except:
+            print("ERROR: You need to be signed in to use SCL.")
+            quit()
