05696d0b50038eddd27dbaa33b62dfe9f4b5b4b3
Michael Halpin <halpinm2010@gmail.com>
2022-04-20 15:21:47 +0100

    .

--- index.py
+++ index.py
@@ -1,10 +1,18 @@
 import scratchconnect
 from sys import argv
 if argv[1] == "sign-in":
-    user = scratchconnect.ScratchConnect(argv[2], argv[3])
-    userw = open("signed-in-user", 'w')
-    userw.write(argv[2] + " " + argv[3])
-    userw.close()
+    if argv.__len__() == 4:
+        try:
+            user = scratchconnect.ScratchConnect(argv[2], argv[3])
+            userw = open("signed-in-user", 'w')
+            userw.write(argv[2] + " " + argv[3])
+            userw.close()
+        except:
+            print("ERROR: Incorrect password or username.")
+            quit()
+    else:
+        print("ERROR: Incorrect usage, scl sign-in <username> <password>.")
+        quit()
 else:
     try:
         userr = open("signed-in-user", 'r')
